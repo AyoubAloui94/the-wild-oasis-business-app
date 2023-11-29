f307d79d23351e6d41e7d207274c1db30139664f
KaneWatson <webitgenius@gmail.com>
2023-11-29 06:29:14 +0100

    cleared initial login state

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin"
 import SpinnerMini from "../../ui/SpinnerMini"
 
 function LoginForm() {
-  const [email, setEmail] = useState("ayoub@example.com")
-  const [password, setPassword] = useState("pass1234")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const { login, isLoggingIn } = useLogin()
 
   function handleSubmit(e) {
