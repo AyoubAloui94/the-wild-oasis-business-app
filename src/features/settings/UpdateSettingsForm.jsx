521cde10389b017b5943095fe5388892d6f90e1d
KaneWatson <webitgenius@gmail.com>
2023-12-14 17:51:30 +0100

    set min for settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -20,16 +20,16 @@ function UpdateSettingsForm() {
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} disabled={isUpdating} onBlur={e => handleUpdate(e, "minBookingLength")} />
+        <Input min={1} type="number" id="min-nights" defaultValue={minBookingLength} disabled={isUpdating} onBlur={e => handleUpdate(e, "minBookingLength")} />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} disabled={isUpdating} onBlur={e => handleUpdate(e, "maxBookingLength")} />
+        <Input min={2} type="number" id="max-nights" defaultValue={maxBookingLength} disabled={isUpdating} onBlur={e => handleUpdate(e, "maxBookingLength")} />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" defaultValue={maxGuestsPerBooking} disabled={isUpdating} onBlur={e => handleUpdate(e, "maxGuestsPerBooking")} />
+        <Input min={1} type="number" id="max-guests" defaultValue={maxGuestsPerBooking} disabled={isUpdating} onBlur={e => handleUpdate(e, "maxGuestsPerBooking")} />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" defaultValue={breakfastPrice} disabled={isUpdating} onBlur={e => handleUpdate(e, "breakfastPrice")} />
+        <Input min={1} type="number" id="breakfast-price" defaultValue={breakfastPrice} disabled={isUpdating} onBlur={e => handleUpdate(e, "breakfastPrice")} />
       </FormRow>
     </Form>
   )
