864cd345b3bd0df94b6b38e1b9f212307effa417
KaneWatson <webitgenius@gmail.com>
2024-07-16 21:01:45 +0200

    add copyright text on the landing page to prevent people from using my project on their portfolio sites (which already happened..)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import LoginForm from "../features/authentication/LoginForm"
 import Logo from "../ui/Logo"
 import Heading from "../ui/Heading"
+import { Link } from "react-router-dom"
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -13,10 +14,27 @@ const LoginLayout = styled.main`
   background-color: var(--color-grey-50);
 `
 
+const Disclaimer = styled.div`
+  text-align: center;
+  text-decoration: underline;
+  text-underline-offset: 0.5rem;
+  font-size: 2rem;
+  transition: all 0.3s;
+
+  &:hover {
+    color: var(--color-blue-100);
+  }
+`
+
 function Login() {
   return (
     <LoginLayout>
       <Logo />
+      <Disclaimer>
+        <Link to={"https://alouiayoub.com"} target="_blank">
+          Portfolio project of Ayoub Aloui{" "}
+        </Link>
+      </Disclaimer>
       <Heading as="h4">Log into your account</Heading>
       <LoginForm />
     </LoginLayout>
